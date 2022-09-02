b651b395ef947578d7ada012cf64e76f10a9383e
yushaolong <yushaolong@360.cn>
2022-09-03 01:26:50 +0800

    add batch concurrent test case

--- ycache_test.go
+++ ycache_test.go
@@ -15,15 +15,16 @@ var bizCacheIns *YInstance
 
 func TestMain(m *testing.M) {
 	redisConf := &RedisConfig{
-		MaxTry:       2,
-		WriteAddr:    redisHost,
-		ReadAddr:     redisHost,
-		Password:     redisPwd,
-		ReadTimeout:  1,
-		WriteTimeout: 1,
-		ConnTimeout:  1,
-		MaxIdleConns: 5,
-		IdleTimeout:  10,
+		MaxTry:         2,
+		WriteAddr:      redisHost,
+		ReadAddr:       redisHost,
+		Password:       redisPwd,
+		ReadTimeout:    1,
+		WriteTimeout:   1,
+		ConnTimeout:    3,
+		MaxActiveConns: 64,
+		MaxIdleConns:   5,
+		IdleTimeout:    10,
 	}
 	collectorConf := &WarmCollectorConfig{
 		BuffSeconds: 10,
@@ -191,20 +192,22 @@ func TestYInstanceStrategy(t *testing.T) {
 	}
 	stat, _ := json.Marshal(bizCacheIns.Stat())
 	t.Logf("ins stat:%s", string(stat))
-
-	key2 := "k2"
-	mp, _ := bizCacheIns.BatchGet(context.Background(), "_abc_", []string{key2}, func(ctx context.Context, nKeys []string) (map[string][]byte, error) {
-		ret := make(map[string][]byte)
-		for _, key := range nKeys {
-			if key == key1 {
-				ret[key] = []byte("batch new k1")
-			} else if key == key2 {
-				ret[key] = []byte("batch new k2")
+	time.Sleep(time.Second * 5)
+	for i := 0; i < 10; i++ {
+		key2 := "k2"
+		mp, _ := bizCacheIns.BatchGet(context.Background(), "_abc_", []string{key2}, func(ctx context.Context, nKeys []string) (map[string][]byte, error) {
+			ret := make(map[string][]byte)
+			for _, key := range nKeys {
+				if key == key1 {
+					ret[key] = []byte("batch new k1")
+				} else if key == key2 {
+					ret[key] = []byte("batch new k2")
+				}
 			}
-		}
-		return ret, nil
-	})
-	t.Logf("instance get 2 success, key2:%s,val:%s", key2, mp)
+			return ret, nil
+		})
+		t.Logf("instance get 2 success, key2:%s,val:%s", key2, mp)
+	}
 	time.Sleep(time.Second * 30)
 	val, err := bizCacheIns.Get(context.Background(), "_abc_", key1, nil)
 	if err != nil {
@@ -225,13 +228,13 @@ func TestYInstanceStrategy(t *testing.T) {
 }
 
 func TestYInstanceConcurrent(t *testing.T) {
-	key1 := "k18"
+	key1 := "k15"
 	for i := 0; i < 20; i++ {
 		i := i
 		go func() {
 			val, _ := bizCacheIns.Get(context.Background(), "_abc_", key1, func(ctx context.Context, key string) ([]byte, error) {
 				time.Sleep(time.Second)
-				fmt.Println("in here", i)
+				fmt.Println("get in here", i)
 				if i < 10 {
 					return []byte("k1 hello"), nil
 				} else {
@@ -241,10 +244,29 @@ func TestYInstanceConcurrent(t *testing.T) {
 			t.Logf("instance get 1 success, index:%d,key:%s,val:%s", i, key1, string(val))
 		}()
 	}
+	key2 := "k25"
+	for i := 0; i < 5; i++ {
+		i := i
+		go func() {
+			kvs, _ := bizCacheIns.BatchGet(context.Background(), "_abc_", []string{key2, key1}, func(ctx context.Context, keys []string) (map[string][]byte, error) {
+				time.Sleep(time.Second)
+				fmt.Println("batch in here ", i)
+				return map[string][]byte{
+					key1: []byte("k1 batch"),
+					key2: []byte("k2 batch"),
+				}, nil
+			})
+			mp := make(map[string]string)
+			for k, v := range kvs {
+				mp[k] = string(v)
+			}
+			t.Logf("instance get 2 success, index:%d,kvs:%v", i, mp)
+		}()
+	}
 	stat, _ := json.Marshal(bizCacheIns.Stat())
 	t.Logf("ins stat:%s", string(stat))
-	time.Sleep(time.Second * 20)
+	time.Sleep(time.Second * 10)
 	stat, _ = json.Marshal(bizCacheIns.Stat())
 	t.Logf("ins stat:%s", string(stat))
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 1)
 }
